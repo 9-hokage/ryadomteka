40d26df04cc8faf58e3798d0319e561e6653a6c6
Adil Khamidullov <53276080+adil2604@users.noreply.github.com>
2021-03-14 00:08:49 +0600

    Create 5.sql

---
+++ sql/5.sql
@@ -0,0 +1,5 @@
+select drugs.description
+from drugs 
+inner join drugs_storedrugs d on d.drug_id=drugs.ID
+inner join stores s ON s.id=d.store_id
+where s.city_id=(select id from ryadomteka_cities where name='Алматы') and s.name='Аптека';
